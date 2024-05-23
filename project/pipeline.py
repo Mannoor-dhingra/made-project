a51ccedf03fe1e624652491ab8be050b549806d7
Mannoor Dhingra <mannoordhingra@gmail.com>
2024-05-23 13:06:14 +0200

    Project work 3

---
+++ project/pipeline.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+import requests
+import zipfile
+import sqlite3
+import io
+
+def ensure_directory(path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except Exception as e:
+            print(f"Error creating directory {path}: {e}")
+            exit(1)
+
+def download_and_transform_dataset1(url, output_folder, db_path):
+    ensure_directory(output_folder)
+    response = requests.get(url)
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+        zip_file.extractall(output_folder)
+    csv_filename = os.path.join(output_folder, zip_file.namelist()[1])
+    df = pd.read_csv(csv_filename, on_bad_lines='skip')
+    df.drop(columns=['Country Code'], inplace=True)
+    df = df.loc[~(df==0).all(axis=1)]
+    df.dropna(inplace=True)
+    ensure_directory(os.path.dirname(db_path))
+    conn = sqlite3.connect(db_path)
+    df.to_sql('world_forest_data', conn, if_exists='replace', index=False)
+    conn.close()
+
+def download_and_transform_dataset2(url, output_folder, db_path):
+    ensure_directory(output_folder)
+    response = requests.get(url)
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+        zip_file.extractall(output_folder)
+    csv_filename = os.path.join(output_folder, zip_file.namelist()[0])
+    df = pd.read_csv(csv_filename, on_bad_lines='skip')
+    df.drop(columns=['1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989'], inplace=True)
+    df = df.loc[~(df==0).all(axis=1)]
+    df.dropna(inplace=True)
+    ensure_directory(os.path.dirname(db_path))
+    conn = sqlite3.connect(db_path)
+    df.to_sql('global_temperature_data', conn, if_exists='replace', index=False)
+    conn.close()
+
+def main():
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Moves up two levels from current script's directory
+    data_dir = os.path.join(base_dir, 'data')
+    project_dir = os.path.join(base_dir, 'project')
+
+    forest_data_url = 'https://www.kaggle.com/api/v1/datasets/download/webdevbadger/world-forest-area'
+
+    temperature_data_url = 'https://www.kaggle.com/api/v1/datasets/download/mdazizulkabirlovlu/all-countries-temperature-statistics-1970-2021?datasetVersionNumber=1'
+
+
+    download_and_transform_dataset1(forest_data_url, os.path.join(project_dir, 'forest_data'), os.path.join(data_dir, 'world_forest_data.sqlite'))
+    download_and_transform_dataset2(temperature_data_url, os.path.join(project_dir, 'temperature_data'), os.path.join(data_dir, 'global_temperature_data.sqlite'))
+
+if __name__ == "__main__":
+    main()
+
+     
--- project/project-plan.md
+++ project/project-plan.md
@@ -1,7 +1,23 @@
 # Project Plan
 
 ## Title
-Climate Change
+Exploring the relationship between world forest data with global temperature data
+
+## Main Question
+How does the change in forest areas affect the global temperatures on a yearly basis?
+
+## Datasources
+### 1. World Forest Area (km and %)
+- **Source**: [World Forest Area](https://www.kaggle.com/datasets/webdevbadger/world-forest-area)
+- **Data Type**: CSV
+- **License**: World Bank Dataset
+- **Description**: This dataset contains information about the world's forest area changes since 1990. Studying global forest area changes is crucial for assessing environmental health, informing conservation strategies, and understanding the impact of human activities on biodiversity and climate regulation.
+
+### 2. All Countries Temperature Statistics 1970-2021
+- **Source**: [All Countries Temperature Statistics](https://www.kaggle.com/datasets/mdazizulkabirlovlu/all-countries-temperature-statistics-1970-2021)
+- **Data Type**: CSV
+- **License**: CC0: Public Domain
+- **Description**: This dataset provides information on changes in global surface temperature across all countries from 1970 to 2021. It includes data on temperature variations over a 51-year period and is based on information from various sources, including weather stations, satellites, and ocean buoys. 
 
 ## Work Packages
 1. Data Acquisition
